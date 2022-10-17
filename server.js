9c3c959253c28824cbc45e66b9a6bfad724cacde
faisaljavedshah <acorelabs@gmail.com>
2022-10-17 13:18:52 +0500

    added three files

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
+//Importing dependencies
 const express = require('express');
 var path = require('path');
-const compression = require('compression')
+const port = process.env.PORT || 8081;
 
 //Starting Express app
 const app = express();
-app.use(compression())
 
 //Set the base path to the angular-test dist folder
-app.use(express.static(path.join(__dirname, 'dist/admin-panel-layout')));
+app.use(express.static(path.join(__dirname, '/')));
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/admin-panel-layout/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 //Starting server on port 8081
-app.listen(8081, () => {
-    console.log('Server started!');
-    console.log('on port 8081');
-});
+app.listen(port, () => {
+    console.log("Server is listening on: ", port);
+  });
